e1632a3f157adf2d417f2b0e3367df2c4a532553
PLAYLANDs-World <myworkspace022@gmail.com>
2025-06-25 23:10:34 +0530

    Update header.js

--- script/header.js
+++ script/header.js
@@ -3,8 +3,8 @@ function resetNetflixHeader() {
 
     if (headerDiv) {
         headerDiv.innerHTML = `
-                <a href="/"><img src="https://cdn.jsdelivr.net/gh/Unknownyy00/01-img-share@main/Netflix-Symbol-RGB.png" class="prime-logo"></a>
-                <a href="/search" class="netflix-search-btn-for-open-search">
+                <a href="/netflix"><img src="https://cdn.jsdelivr.net/gh/Unknownyy00/01-img-share@main/Netflix-Symbol-RGB.png" class="prime-logo"></a>
+                <a href="/netflix/search" class="netflix-search-btn-for-open-search">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                     </svg>
@@ -20,7 +20,7 @@ function resetNetflixHeader() {
                                 <li><a href=""><i class="fas fa-user"></i> My Profile</a></li>
                                 <li><a href=""><i class="fas fa-cog"></i> Account Settings</a></li>
                                 <li class="divider"></li>
-                                <li><a href="/"><i class="fas fa-home"></i> Home</a></li>
+                                <li><a href="/netflix"><i class="fas fa-home"></i> Home</a></li>
                                 <li><a href=""><i class="fas fa-film"></i> Movies</a></li>
                                 <li><a href=""><i class="fas fa-tv"></i> TV Shows</a></li>
                                 <li class="divider"></li>
